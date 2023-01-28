693970670ceabbbbe3e764dbd1bd53dafde443ca
xlSaladix <neyencoria@gmail.com>
2023-01-28 14:50:58 -0300

    NavBar Terminado, Inicio Terminado. v0.2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Rutas - routerLinks
+
+import { InicioComponent } from './components/inicio/inicio.component';
+import { InfoComponent } from './components/info/info.component';
+import { DonacionesComponent } from './components/donaciones/donaciones.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FilesComponent } from './components/files/files.component';
+
+const routes: Routes = [
+  {path: 'inicio', title: 'L2Name', component:InicioComponent},
+  {path: 'info', title: 'L2Name Informacion', component:InfoComponent},
+  {path: 'registro', title: 'L2Name Registro', component:RegistroComponent},
+  {path: 'files', title: 'L2Name Files', component:FilesComponent},
+  {path: 'donaciones', title: 'L2Name Donaciones', component:DonacionesComponent},
+  {path: 'contact', title: 'L2Name Contacto', component:ContactComponent},
+  {path: '', redirectTo: '/inicio', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { DonacionesComponent } from './components/donaciones/donaciones.component';
+import { StreamsComponent } from './components/streams/streams.component';
+import { InfoComponent } from './components/info/info.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FilesComponent } from './components/files/files.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DonacionesComponent,
+    StreamsComponent,
+    InfoComponent,
+    InicioComponent,
+    FooterComponent,
+    RegistroComponent,
+    ContactComponent,
+    FilesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
