3f672c05540691665c4c7a8698988509946062fe
xlSaladix <neyencoria@gmail.com>
2023-01-29 17:25:29 -0300

    Seccion Registro Terminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -30,7 +31,9 @@ import { FilesComponent } from './components/files/files.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/registro/registro.component.ts
+++ src/app/components/registro/registro.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
   styleUrls: ['./registro.component.css']
 })
-export class RegistroComponent {
+export class RegistroComponent implements OnInit {
+  form: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.form= this.formBuilder.group({
+      username:['', [Validators.required, Validators.minLength(4)]],
+      password:['', [Validators.required, Validators.minLength(8)]],
+      email:['', [Validators.required, Validators.email, Validators.minLength(10)]],
+    })
+  }
+
+  ngOnInit() {}
+
+  get Password() {
+    return this.form.get("password");
+  }
+
+  get Username() {
+    return this.form.get("username");
+  }
+
+  get Mail() {
+    return this.form.get("email");
+  }
+
+  get UsernameValid() {
+    return this.Username?.touched && !this.Username?.valid;
+  }
+
+  get PasswordValid() {
+    return this.Password?.touched && !this.Password?.valid;
+  }
+
+  get MailValid() {
+    return false 
+  }
+
+  onEnviar(event: Event) {
+    event.preventDefault;
+
+    if (this.form.valid) {
+      alert("Todo salio bien ¡Enviar formuario!")
+    } else {
+      this.form.markAllAsTouched();
+    }
+  }
 }
