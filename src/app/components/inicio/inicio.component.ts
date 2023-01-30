2bcfa54194191e2966494e805b4bc2220c533062
xlSaladix <neyencoria@gmail.com>
2023-01-30 00:44:48 -0300

    Front Terminado v1.0

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+// Icons FB + WSP
+
+import { faWhatsapp, faFacebook } from '@fortawesome/free-brands-svg-icons';
+
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent {
-
+  faWhatsapp = faWhatsapp;
+  faFacebook = faFacebook;
 }
