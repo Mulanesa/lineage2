eb437348c244bc25dc42c82de78ec559e27d3a9e
xlSaladix <neyencoria@gmail.com>
2023-01-30 23:08:56 -0300

    Pagina terminada con conexion al panel

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+// Icons
+import { faWhatsapp, faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
-
+  faWhatsapp = faWhatsapp;
+  faFacebook = faFacebook;
+  faInstagram = faInstagram;
 }
--- src/app/components/registro/registro.component.ts
+++ src/app/components/registro/registro.component.ts
@@ -8,6 +8,8 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class RegistroComponent implements OnInit {
   form: FormGroup;
+  form2: FormGroup;
+  form3: FormGroup;
 
   constructor(private formBuilder: FormBuilder) {
     this.form= this.formBuilder.group({
@@ -15,9 +17,19 @@ export class RegistroComponent implements OnInit {
       password:['', [Validators.required, Validators.minLength(8)]],
       email:['', [Validators.required, Validators.email, Validators.minLength(10)]],
     })
+    this.form2= this.formBuilder.group({
+      email:['', [Validators.required, Validators.email, Validators.minLength(10)]],
+    })
+    this.form3= this.formBuilder.group({
+      username:['', [Validators.required, Validators.minLength(4)]],
+      password:['', [Validators.required, Validators.minLength(8)]],
+      email:['', [Validators.required, Validators.email, Validators.minLength(10)]],
+    })
   }
 
   ngOnInit() {}
+
+    // Form | 1
 
   get Password() {
     return this.form.get("password");
@@ -43,13 +55,75 @@ export class RegistroComponent implements OnInit {
     return false 
   }
 
-  onEnviar(event: Event) {
+// ************************************************************
+
+  // Form | 2
+
+  get Mail2() {
+    return this.form2.get("email");
+  }
+
+  get Mail2Valid() {
+    return false 
+  }
+
+// ************************************************************
+
+  // Form | 3
+
+  get Password3() {
+    return this.form3.get("password");
+  }
+
+  get Username3() {
+    return this.form3.get("username");
+  }
+
+  get Mail3() {
+    return this.form3.get("email");
+  }
+
+  get UsernameValid3() {
+    return this.Username?.touched && !this.Username?.valid;
+  }
+
+  get PasswordValid3() {
+    return this.Password?.touched && !this.Password?.valid;
+  }
+
+  get MailValid3() {
+    return false 
+  }
+
+// ************************************************************
+
+  onEnviar(event: Event) { // Form | 1
     event.preventDefault;
 
     if (this.form.valid) {
       alert("Todo salio bien ¡Enviar formuario!")
     } else {
       this.form.markAllAsTouched();
+    }
+  }
+
+  onEnviarF2(event: Event) { // Form | 2
+    event.preventDefault;
+
+    if (this.form2.valid) {
+      alert("Todo salio bien ¡Enviar formuario!")
+    } else {
+      this.form2.markAllAsTouched();
+    }
+  }
+
+  onEnviarF3(event: Event) { // Form | 3
+    event.preventDefault;
+
+    if (this.form3.valid) {
+      alert("Todo salio bien ¡Enviar formuario!")
+    } else {
+      this.form3.markAllAsTouched();
     }
   }
 }
